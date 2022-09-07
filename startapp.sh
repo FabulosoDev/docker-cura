e15c816c43e141fdeb20386387844810203af01a
Janneck <janneck.denda@gmail.com>
2022-09-07 13:45:05 +0200

    xycsyxcyxcyxc

--- startapp.sh
+++ startapp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec LD_PRELOAD=/usr/lib/libstdc++.so.6 QT_QPA_PLATFORM=xcb QT_STYLE_OVERRIDE="" /usr/share/cura/Ultimaker_Cura.AppImage --appimage-extract-and-run .
+exec6 QT_QPA_PLATFORM=xcb QT_STYLE_OVERRIDE="" /usr/share/cura/Ultimaker_Cura.AppImage --appimage-extract-and-run .
