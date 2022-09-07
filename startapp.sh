f85b8990a27d302622784b14988f6eb3efccbd38
Janneck <janneck.denda@gmail.com>
2022-09-07 13:45:30 +0200

    dsafsdf

--- startapp.sh
+++ startapp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec6 QT_QPA_PLATFORM=xcb QT_STYLE_OVERRIDE="" /usr/share/cura/Ultimaker_Cura.AppImage --appimage-extract-and-run .
+exec QT_QPA_PLATFORM=xcb QT_STYLE_OVERRIDE="" /usr/share/cura/Ultimaker_Cura.AppImage --appimage-extract-and-run .
